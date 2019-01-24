2e13b31bdb56eae1bf5fc3f57de6b2b4ac83aafc
devnoorfatima <44938970+devnoorfatima@users.noreply.github.com>
2019-01-24 11:56:36 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function palindrome(str) {
     return  str.replace(/[\W_]/g, '').toLowerCase() === 
             str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join(''); 
     }
+
+    function check(){
+        var value = document.getElementById("input").value;
+        document.getElementById("p").innerHTML=palindrome(value);
+    }
+
+
     
-   console.log( palindrome("racecar"));
-   console.log(palindrome("1 eye for of 1 eye."));
