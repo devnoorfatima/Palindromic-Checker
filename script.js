51e80ca5b59e72d31627e0116894d1ae7d76ba93
devnoorfatima <44938970+devnoorfatima@users.noreply.github.com>
2019-02-03 17:06:58 +0500

    Added a function.
    Added a function by which pressing the enter key calls the function.

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function palindrome(str) {
-    return  str.replace(/[\W_]/g, '').toLowerCase() === 
-            str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join(''); 
+    return str.replace(/[\W_]/g, '').toLowerCase() ===
+        str.replace(/[\W_]/g, '').toLowerCase().split('').reverse().join('');
+}
+const input = document.getElementById("input");
+
+function check() {
+    let value = input.value;
+    document.getElementById("p").innerHTML = palindrome(value);
+}
+input.addEventListener("keypress", function (x) {
+    if (x.keyCode === 13) {
+        check();
     }
-
-    function check(){
-        var value = document.getElementById("input").value;
-        document.getElementById("p").innerHTML=palindrome(value);
-    }
-
-
-    
+});
